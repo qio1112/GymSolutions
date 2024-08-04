b36c4d1e33e9fb5019bca5fb72aea920d938f007
Yipeng Zhao <yipeng6257@gmail.com>
2024-08-04 00:10:41 -0400

    add feature of using a layer as std for lunarlander continuous

--- lunar_lander/policy_continuous.py
+++ lunar_lander/policy_continuous.py
@@ -8,15 +8,17 @@ from utils.utils import get_device
 
 
 class LinearNetwork(nn.Module):
-    def __init__(self, state_dimension, action_dimension, fixed_std=None):
+    def __init__(self, state_dimension, action_dimension, param_std=True):
         super(LinearNetwork, self).__init__()
-        self.fixed_std = fixed_std
+        self.param_std = param_std
         self.fc1 = nn.Linear(state_dimension, 64)
         self.fc2 = nn.Linear(64, 128)
         self.fc3 = nn.Linear(128, 64)
         self.mean_layer = nn.Linear(64, action_dimension)
-        if fixed_std is None:
+        if self.param_std:
             self.log_std = nn.Parameter(torch.zeros(action_dimension))
+        else:
+            self.log_std = nn.Linear(64, action_dimension)
         # self.init_weights()
 
     def forward(self, x):
@@ -24,10 +26,10 @@ class LinearNetwork(nn.Module):
         x = torch.relu(self.fc2(x))
         x = torch.relu(self.fc3(x))
         mean = torch.tanh(self.mean_layer(x))
-        if self.fixed_std is None:  # use parameterized std
-            std = torch.exp(self.log_std)  # to make sure std is positive
+        if self.param_std:
+            std = torch.exp(self.log_std)
         else:
-            std = torch.full_like(mean, self.fixed_std)
+            std = torch.exp(self.log_std(x))
         return mean, std
 
     def init_weights(self):
@@ -82,7 +84,7 @@ if __name__ == "__main__":
     config = {
         "path": path,
         "log_path": log_path,
-        "fixed_std": None,  # use fixed std for Gaussian distributions, set to None or don't set it to use trained std
+        "param_std": False,  # if true, use nn.Parameter for std, else use a layer
         "discrete": False,
         "train": True,
         "use_baseline": True,
@@ -94,13 +96,13 @@ if __name__ == "__main__":
         "lr": 5e-4,
         "num_batches": 2000,  # number of sampling and train times
         "batch_size": 500,  # steps in each batch, the last episode may exceed this size
-        "max_ep_len": 1000,  # max length of each episode
+        "max_ep_len": 3000,  # max length of each episode
         "early_stop_reward": 200,  # early stop when average reward reaches this value. set to None or 0 to disable
         "early_stop_ma": 50  # window size of moving average for early stop
     }
 
     # continuous action has 2 dimensions with range [-1, 1], so use tanh
-    policy_network = LinearNetwork(observation_dim, action_dim, fixed_std=config.get("fixed_std", None))
+    policy_network = LinearNetwork(observation_dim, action_dim, config.get("param_std", True))
     baseline_network = LinearNetworkBaseline(observation_dim)
 
     p_agent = PAgent(config, env, policy_network, baseline_network, device)
